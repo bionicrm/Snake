c2c903c00d2ebd9f44314bc24adf1255a900b747
T.J. Mazeika <t@mazeika.me>
2015-12-04 21:27:47 -0500

    Make render loop

--- public/src/bin/main.dart
+++ public/src/bin/main.dart
@@ -1,3 +1,23 @@
+import 'dart:html';
+
+final CanvasElement canvas = querySelector('#canvas');
+final CanvasRenderingContext2D ctx = canvas.context2D;
+final int width = canvas.width = window.innerWidth;
+final int height = canvas.height = window.innerHeight;
+
 void main() {
-  print('Hello, world.');
+  window.animationFrame.then(render);
+}
+
+void render(double now) {
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.beginPath();
+  ctx.setStrokeColorRgb(255, 0, 0);
+  ctx.lineWidth = 5;
+  ctx.moveTo(0, 0);
+  ctx.lineTo(width, height);
+  ctx.stroke();
+
+  window.animationFrame.then(render);
 }
